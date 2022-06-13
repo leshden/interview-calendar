182bfb941d7f2c307f185247e232ba40baa5e30b
Viktor Denikin <leshden@yandex.ru>
2022-06-13 22:26:40 +0300

    month-year

--- src/components/calendar-panel/CalendarPanel.js
+++ src/components/calendar-panel/CalendarPanel.js
@@ -4,11 +4,16 @@ import Fragment from 'react';
 const CalendarPanel = () => {
   return (
       <div className='calendar-days-container'>
-        <div className='days-symbols'>
+        <div className='row-days-symbols'>
           M T W T F S S
         </div>
-        <div className='days-numbers'>
+        <div className='row-days-numbers'>
           1 2 3 4 5 6 7
+        </div>
+        <div className='row-month-year-container'>
+          <button>&#10094;</button>
+          <div>March 2019</div>
+          <button>&#10095;</button>
         </div>
       </div>
   )
