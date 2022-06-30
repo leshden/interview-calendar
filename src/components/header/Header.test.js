dd34103038ca26622f2a08788a72194abbd2195e
Viktor Denikin <leshden@yandex.ru>
2022-06-30 21:58:31 +0300

    test header click button

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import Header from './Header';
 
 test('render Header', () => {
   render(<Header />);
   const textElement = screen.getByText(/calendar/i);
   expect(textElement).toBeInTheDocument();
+});
+
+test('click AddEvent ', () => {
+  render(<Header />);
+  const addButton = screen.getByRole('button');
+  userEvent.click(addButton);
+  expect(addButton).toBeInTheDocument();
 });
